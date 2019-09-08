2f36dbd8db43384f9fbccf9df2af4fe90e4f2e74
Daniël Haveman <daniel.haveman@indg.com>
2019-09-08 18:11:42 +0200

    Updated app icon to have to current MSX colors

---
+++ build/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./getconfigtic.sh; make -j4; open bin/tic80.app
---
+++ build/getconfigtic.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cp /Users/solitude/Library/Application\ Support/com.nesbox.tic/TIC-MSX/config.tic ..
+pushd .
+cd /Users/solitude/Library/Application\ Support/com.nesbox.tic/TIC-MSX
+rm -Rf .local
+popd
